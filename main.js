577081701eef61a69d02b09a8950fd72f6359f36
maariioxx <mariorb2017@gmail.com>
2023-11-01 13:53:54 +0100

    Create clear function

--- main.js
+++ main.js
@@ -1,15 +1,94 @@
-function add(x, y){
-    return x + y;
+let number1 = 0;
+let number2 = 0;
+let operator;
+let operatorClicked = false;
+
+
+function add(number1, number2){
+    return number1 + number2;
 }
 
-function multiply(x, y){
-    return x * y;
+function multiply(number1, number2){
+    return number1 * number2;
 }
 
-function subtract(x, y){
-    return x - y;
+function subtract(number1, number2){
+    return number1 - number2;
 }
 
-function divide(x, y){
-    return x / y;
+function divide(number1, number2){
+    return number1 / number2;
 }
+
+function operate(number1, number2){
+    switch (operator) {
+        case "+": return add(number1, number2);
+        case "-": return subtract(number1, number2);
+        case "*": return multiply(number1, number2);
+        case "/": return divide(number1, number2);
+    }
+};
+
+const numberButtons = document.querySelectorAll(".number");
+const input = document.querySelector(".input");
+const output = document.querySelector(".output");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalButton = document.querySelector(".equal");
+const clearButton = document.querySelector(".clear")
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        if(input.textContent === "0"){
+        input.textContent = button.innerHTML;
+    } else {
+        input.textContent += button.innerHTML
+    };
+    });
+});
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        if(output.textContent !== ""){
+            input.textContent = output.textContent
+            output.textContent = "";
+            operatorClicked = false;
+        }
+        operator = button.innerHTML;
+        updateDisplay();
+        operatorClicked = true;
+        
+    })
+});
+
+equalButton.addEventListener("click", () => {
+    if(operator === undefined){
+        output.textContent = input.innerHTML;
+    } else {
+        getValues()
+        output.textContent = operate(number1, number2)   
+    };
+});
+
+clearButton.addEventListener("click", clear);
+
+function updateDisplay(){
+    if(operatorClicked === false){
+        input.textContent += operator;
+        console.log(operator)
+    }
+    };
+
+function getValues(){
+    values = input.textContent.split(operator);
+    number1 = Number(values[0]);
+    number2 = Number(values[1]);
+}
+
+function clear(){
+    number1 = "";
+    number2 = "";
+    operator = "";
+    input.textContent = "";
+    output.textContent = "";
+}
+
